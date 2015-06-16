de86fdbe7544e6b40d907496c839deabae645634
wataken44 <wataken44@gmail.com>
2015-06-16 16:53:34 +0900

    [F] delete log older than 8 days

--- handler/clean_task.py
+++ handler/clean_task.py
@@ -6,6 +6,8 @@
 
 """
 
+from datetime import datetime, timedelta
+
 import webapp2
 from google.appengine.ext import db
 
@@ -13,7 +15,9 @@ from model.performance_log_model import PerformanceLogModel
 
 class CleanTask(webapp2.RequestHandler):
     def get(self):
+        th = datetime.now() - timedelta(8) # 8 days
         query = PerformanceLogModel.all(keys_only=True)
+        query.filter('appended <', th)
         query.order('appended')
 
         logs = query.fetch(limit = 120)
