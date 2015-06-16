5048aa270110eadc126ec64b035e70982b1355c5
wataken44 <wataken44@gmail.com>
2015-06-16 16:09:18 +0900

    [F] mask ip address

--- handler/history_page.py
+++ handler/history_page.py
@@ -14,8 +14,25 @@ class HistoryPage(BasePage):
         query = PerformanceLogModel.all()
         query.order('-appended')
         logs = query.fetch(limit=20)
-
+        
+        for i in range(len(logs)):
+            logs[i].remote_addr = self._mask(logs[i].remote_addr)
+        
         self.response.out.write(self._render('history.html', {'logs' : logs}))
 
+    def _mask(self, addr):
+        if addr.find('.') >= 0: #ipv4
+            arr = addr.split(".")
+            arr[-1] = 'x'
+            arr[-2] = 'y'
+            return '.'.join(arr)
+        elif addr.find(':'): #ipv6
+            arr = addr.split(":")
+            arr[0] = 'y'
+            arr[1] = 'x'
+            return ':'.join(arr)
+        else:
+            return 'xxxxxxxx'
+        
 if __name__ == "__main__":
     pass
